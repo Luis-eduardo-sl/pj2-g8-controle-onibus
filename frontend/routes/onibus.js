304f3f5d274a6694f2491508fc10c51940780410
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2023-10-22 19:30:22 -0300

    reestruturação front , começo  implementação axios

---
+++ frontend/routes/onibus.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/listar", function (req, res, next) {
+  res.render("onibus/listar");
+});
+
+router.get("/cadastrar", function (req, res, next) {
+  res.render("onibus/cadastrar");
+});
+
+router.get("/exibir/:id", function (req, res, next) {
+  res.render("onibus/exibir");
+});
+
+router.get("/editar/:id", function (req, res, next) {
+  res.render("onibus/editar");
+});
+
+router.get("/excluir/:id", function (req, res, next) {
+  res.render("onibus/excluir");
+});
+
+router.get("/qtd-horarios-por-linha", function (req, res, next) {
+  res.render("onibus/qtdHorariosPorLinha");
+});
+
+module.exports = router;
