f0fc40d4954b8e061bbd21805ced890ff673639e
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2023-10-22 22:58:01 -0300

    axios site usuarios

---
+++ frontend/site/cadastrar-usuario.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    displayFlashMessage();
+  
+    const form = document.querySelector("#form");
+  
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+  
+      if (form.checkValidity()) {
+        const nome = document.querySelector("#nome").value;
+        const cpf = document.querySelector("#cpf").value;
+        const telefone = document.querySelector("#telefone").value;
+        const email = document.querySelector("#email").value;
+        const senha = document.querySelector("#senha").value;
+        const tipo = document.querySelector("#tipo").value;
+  
+        const data = { nome, cpf, telefone, email, senha , tipo  };
+  
+        try {
+          const response = await axios.post("http://localhost:3000/api/usuario/cadastrar", data);
+        
+          storeFlashMessage("success", "Cadastro realizado sucesso");
+  
+          const id = response.data.id;
+          window.location.href = `http://localhost:5500/frontend/site/cadastro.html?`;
+        } catch (error) {
+          triggerFlashMessage("danger", error.message);
+        }
+      }
+      
+      form.classList.add("was-validated");
+    });
+  });
