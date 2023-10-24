99f37839deedb392a186551d72a8dbe15590a6fb
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2023-10-24 15:53:31 -0300

    começo reformulação frontend

--- frontend-design/app.js
+++ frontend-design/app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const onibusRouter = require('./routes/onibus');
 
-var app = express();
+const motoristasRouter = require('./routes/motoristas');
+const linhasRouter = require('./routes/linhas');
+const usuariosRouter = require('./routes/usuarios');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/onibus', onibusRouter);
+
+app.use('/motoristas', motoristasRouter);
+app.use('/linhas', linhasRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
